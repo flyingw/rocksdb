cc3ef1c68ce1ce1b2f2fb0f754c566a178fc335a
--local <--local>
2025-07-23 15:14:50 +0300

    export multi scan

--- db/c.cc
+++ db/c.cc
@@ -59,6 +59,7 @@
 #include "rocksdb/write_batch.h"
 #include "rocksdb/write_buffer_manager.h"
 #include "rocksdb/wide_columns.h"
+#include "rocksdb/multi_scan.h"
 #include "db/wide/wide_column_serialization.h"
 #include "rocksdb/attribute_groups.h"
 #include "db/attribute_group_iterator_impl.h"
@@ -288,6 +289,9 @@ struct rocksdb_iterator_t {
 struct rocksdb_attributegroup_iterator_t {
   AttributeGroupIterator* rep;
 };
+struct rocksdb_multi_scan_t {
+  MultiScan* rep;
+};
 struct rocksdb_writebatch_t {
   WriteBatch rep;
 };
@@ -404,6 +408,9 @@ struct rocksdb_walfilter_t : public WalFilter {
 };
 struct rocksdb_snapshot_t {
   const Snapshot* rep;
+};
+struct rocksdb_scanoptions_t {
+  ScanOptions rep;
 };
 struct rocksdb_flushoptions_t {
   FlushOptions rep;
@@ -3212,6 +3219,23 @@ rocksdb_attributegroup_iterator_t* rocksdb_create_iterator_attribute_group(
 
   std::unique_ptr<AttributeGroupIterator> iter = db->rep->NewAttributeGroupIterator(options->rep, column_families);
   result->rep= iter.release();
+  return result;
+}
+
+rocksdb_multi_scan_t* rocksdb_create_multi_scan(
+  rocksdb_t* db, const rocksdb_readoptions_t* options,
+  rocksdb_scanoptions_t** scan_options,
+  rocksdb_column_family_handle_t* column_family,
+  size_t size) {
+
+  rocksdb_multi_scan_t* result = new rocksdb_multi_scan_t;
+  std::vector<ScanOptions> scan_opts;
+  for (size_t i = 0; i < size; i++) {
+    scan_opts.push_back(scan_options[i] -> rep);
+  }
+
+  std::unique_ptr<MultiScan> iter = db->rep->NewMultiScan(options->rep, column_family->rep, scan_opts);
+  result -> rep = iter.release();
   return result;
 }
 
